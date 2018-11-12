057d5de5a061801869f99525df075e10cf540522
Alchemy <2457043027@qq.com>
2018-11-12 21:38:02 +0800

    增加@Overide注释

--- ForgeVariable/src/com/extendedclip/papi/expansion/forge/ForgeVariable.java
+++ ForgeVariable/src/com/extendedclip/papi/expansion/forge/ForgeVariable.java
@@ -7,26 +7,32 @@ import org.sct.fv.loreattribute.api.ForgeApi;
 
 public class ForgeVariable extends PlaceholderExpansion {
 
+    @Override
     public boolean canRegister() {
         return Bukkit.getPluginManager().getPlugin(getPlugin()) != null;
     }
 
+    @Override
     public String getAuthor() {
         return "alchemy";
     }
 
+    @Override
     public String getIdentifier() {
         return "Forge";
     }
 
+    @Override
     public String getPlugin() {
         return "LoreAttribute";
     }
 
+    @Override
     public String getVersion() {
         return "1.0";
     }
 
+    @Override
     public String onPlaceholderRequest(Player player, String identifier) {
         if (player == null) {
             return "";
